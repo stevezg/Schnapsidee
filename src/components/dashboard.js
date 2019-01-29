9071a262fdcd8a8799156b6c2fb351d1302436b2
Stephen Anderson <anderssj@usc.edu>
2019-01-29 09:19:53 -0800

    form

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,35 +1,54 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import requiresLogin from './requires-login';
-import {fetchProtectedData} from '../actions/protected-data';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm, focus } from 'redux-form'
+import requiresLogin from './requires-login'
+import { fetchProtectedData } from '../actions/protected-data'
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchProtectedData());
-    }
+  constructor(props) {
+    super(props)
+    this.state = { guess: null }
+  }
+  componentDidMount() {
+    this.props.dispatch(fetchProtectedData())
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+  handleChange = e => {
+    this.setState({ guess: e.target.value })
+  }
+  render() {
+    return (
+      <div className="dashboard">
+        <div className="dashboard-username">: {this.props.username}</div>
 
-    render() {
-        return (
-            <div className="dashboard">
-                <div className="dashboard-username">
-                    Username: {this.props.username}
-                </div>
-                <div className="dashboard-name">Name: {this.props.name}</div>
-                <div className="dashboard-protected-data">
-                    Protected data: {this.props.protectedData}
-                </div>
-            </div>
-        );
-    }
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              value={this.state.guess}
+              onChange={this.handleChange}
+              type="text"
+              name="guess"
+              placeholder="enter the translation"
+            />
+            <button value="Submit" type="submit">
+              ENTER
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    const {currentUser} = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName} ${currentUser.lastName}`,
-        protectedData: state.protectedData.data
-    };
-};
+  const { currentUser } = state.auth
+  return {
+    username: state.auth.currentUser.username,
+    name: `${currentUser.firstName} ${currentUser.lastName}`,
+    protectedData: state.protectedData.data
+  }
+}
 
-export default requiresLogin()(connect(mapStateToProps)(Dashboard));
+export default requiresLogin()(connect(mapStateToProps)(Dashboard))
